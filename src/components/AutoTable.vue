84ed2f9bb3dda5658c889bf790273d420337fb4e
Pierre Delclaux <pimise@gmail.com>
2022-03-24 16:54:40 +0100

    REORG/MAJOR: Deep refactoring, naming and coding style review
    This is a first shot at pplying best practices in an attemps to make
    the code more readable:
    
    - coding style review
    - global v-for cleanup
    - CSS improvements (class names, SCSS)
    - naming improvements
    - some logic removed from HTML

--- src/components/AutoTable.vue
+++ src/components/AutoTable.vue
@@ -3,7 +3,7 @@
         <v-data-table
             :id="id"
             :headers="headers"
-            :items="tableData"
+            :items="formattedTableItems"
             class="auto-table elevation-0"
             :item-class="itemClass"
             dense
@@ -15,116 +15,122 @@
             mobile-breakpoint="0"
             :disable-pagination="!this.isPaginated"
             :hide-default-footer="!this.isPaginated"
-            >
-
+        >
             <template v-slot:body="{ items, headers }">
                 <tbody>
-                    <template v-for="(item, index) in items">
-                        <tr :key="index" :class="typeof itemClass=='function' ? itemClass(item) : itemClass">
-                            <template v-for="(headerItem, headerIndex) in headers">
-                                <td :key="headerIndex" :title="headerItem.cdef.tdTooltip(item[headerItem.value], item)" :class="headerItem.cdef.tdClass(item[headerItem.value], item) + ' v-data-table__divider col_' + headerItem.value">
-                                    <template v-if="headerItem.cdef.html">
-                                        <span :key="headerItem.id" :title="headerItem.cdef.tooltip(item[headerItem.value], item)" :style="headerItem.cdef.style(item[headerItem.value], item)" v-html="headerItem.cdef.format(item[headerItem.value], item)"></span>
-                                    </template>
-                                    <template v-else>
-                                        <span :key="headerItem.id" :title="headerItem.cdef.tooltip(item[headerItem.value], item)" :style="headerItem.cdef.style(item[headerItem.value], item)">{{headerItem.cdef.format(item[headerItem.value], item)}}</span>
-                                    </template>
-                                    <span v-if="activeCopyCellContent && headerItem.cdef.format(item[headerItem.value], item)" class="cp-span mdi mdi-content-copy" @click="copyCellContent('col_'+headerItem.value, index, $event)">
-                                        <span class="cell-copied-tooltip">Copied!</span>
-                                    </span>
-                                </td>
-                            </template>
-                        </tr>
-                    </template>
+                    <tr v-for="item in items" :key="item.id" :class="getRowClass(item)">
+                        <td
+                            v-for="(header, headerIndex) in headers"
+                            :key="headerIndex"
+                            :title="header.columnDefinition.getTitle(item)"
+                            :class="getHeaderClassForItem(header, item)"
+                        >
+                            <span
+                                v-if="header.columnDefinition.isHtml"
+                                :title="header.columnDefinition.getTooltip(item[header.value], item)"
+                                :style="header.columnDefinition.getStyle(item[header.value], item)"
+                                v-html="header.columnDefinition.format(item[header.value], item)"
+                            ></span>
+                            <span
+                                v-else
+                                :title="header.columnDefinition.getTooltip(item[header.value], item)"
+                                :style="header.columnDefinition.getStyle(item[header.value], item)"
+                                >{{ header.columnDefinition.format(item[header.value], item) }}</span
+                            >
+                            <span
+                                v-if="activeCopyCellContent && header.columnDefinition.format(item[header.value], item)"
+                                class="cp-span mdi mdi-content-copy"
+                                @click="copyCellContent('col_' + header.value, headerIndex, $event)"
+                            >
+                                <span class="cell-copied-tooltip">Copied!</span>
+                            </span>
+                        </td>
+                    </tr>
                 </tbody>
             </template>
-
         </v-data-table>
 
-        <v-overlay :absolute="true" :opacity="0.5" color="#ffffff" :value="loading">
-            <v-progress-circular
-                indeterminate
-                size="64"
-                color="#a2a2a2"
-                ></v-progress-circular>
+        <v-overlay :absolute="true" :opacity="0.5" color="#ffffff" :value="isLoading">
+            <v-progress-circular indeterminate size="64" color="#a2a2a2"></v-progress-circular>
         </v-overlay>
-
     </v-card>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .auto-table * {
     font-size: 12.8px;
 }
-
-::v-deep th {
-    padding-left: 6px !important;
-    background-color: #FCFCFC !important;
-    border-top: thin solid rgba(0, 0, 0, 0.02);
-}
-
-::v-deep tbody .v-data-table__divider {
-    padding: 1px 6px !important;
-    height: auto !important;
-    /* position relative needed for the copy button icon */
-    position: relative;
-}
-
-::v-deep tbody .v-data-table__divider:not(.nocp) {
-    padding-right: 18px !important;
-}
-
 tbody .v-data-table__divider span {
     white-space: nowrap;
     display: block;
 }
 
-::v-deep .v-data-table__divider .cp-span {
-    position: absolute;
-    display: none;
-    right: 0;
-    top: 0.7px;
-    padding-top: 1px;
-    padding-right: 2px;
-    cursor: pointer;
-}
+::v-deep {
+    th {
+        padding-left: 6px !important;
+        background-color: #fcfcfc !important;
+        border-top: thin solid rgba(0, 0, 0, 0.02);
+    }
 
-::v-deep .v-data-table__divider:hover:not(.nocp) .cp-span {
-    display: block;
-}
+    tbody {
+        .v-data-table__divider {
+            padding: 1px 6px !important;
+            height: auto !important;
+            /* position relative needed for the copy button icon */
+            position: relative;
 
-::v-deep .v-data-table__divider .cp-span .cell-copied-tooltip {
-    visibility: hidden;
-    background-color: rgb(58, 57, 57);
-    color: #fff;
-    text-align: center;
-    border-radius: 2px;
-    padding: 0px 4px 0px 4px;
-    position: absolute;
-    top: 2px;
-    right: 17px;
-    font-size: 11px;
-}
+            &:not(.nocp) {
+                padding-right: 18px !important;
+            }
 
-::v-deep .v-data-table__wrapper tbody tr:hover:not(.v-data-table__expanded__content) {
-    filter: grayscale(10%) brightness(95%);
+            &:hover:not(.nocp) .cp-span {
+                display: block;
+            }
+
+            .cp-span {
+                position: absolute;
+                display: none;
+                right: 0;
+                top: 0.7px;
+                padding-top: 1px;
+                padding-right: 2px;
+                cursor: pointer;
+
+                .cell-copied-tooltip {
+                    visibility: hidden;
+                    background-color: rgb(58, 57, 57);
+                    color: #fff;
+                    text-align: center;
+                    border-radius: 2px;
+                    padding: 0px 4px 0px 4px;
+                    position: absolute;
+                    top: 2px;
+                    right: 17px;
+                    font-size: 11px;
+                }
+            }
+        }
+
+        tr:hover:not(.v-data-table__expanded__content) {
+            filter: grayscale(10%) brightness(95%);
+        }
+    }
 }
 </style>
 
 <script>
 import { VDataTable } from 'vuetify/lib';
-import axios from 'axios';
-import { copyToClipboard } from "@/plugins/utils";
+import { copyToClipboard, unArray } from '@/plugins/utils';
 
-const default_column_definition = {
+const defaultColumnDefinition = {
     format: (v) => v,
-    style: () => '',
-    tooltip: () => '',
-    html: false,
+    getStyle: () => '',
+    getTooltip: () => '',
+    isHtml: false,
     hidden: false,
-    tdClass: () => '',
-    tdTooltip: () => '',
-}
+    getClass: () => '',
+    getTitle: () => '',
+};
 
 export default {
     name: 'AutoTable',
@@ -133,7 +139,7 @@ export default {
             type: String,
             default: 'auto-table',
         },
-        api:{
+        api: {
             type: String,
             required: true,
         },
@@ -154,62 +160,73 @@ export default {
             type: [String, Number],
             default: 'auto',
         },
-        autoTableHeightExtra: {
+        heightOffsets: {
             type: Array,
-            default: () => ([]),
+            default: () => [],
             /* array of dom ids to substract of +/- numbers */
         },
         itemClass: {
             type: [String, Function],
-            default: () => (''),
+            default: () => '',
         },
-        headersComputation: {
+        customHeadersComputation: {
             type: Function,
             default: () => ({}),
         },
         activeCopyCellContent: {
             type: Boolean,
             default: true,
-        }
+        },
     },
     components: {
         VDataTable,
     },
     computed: {
+        /**
+         * Headers extracted from table content and formatted
+         */
         headers() {
-            /* acquire header keys from data */
-            let headers = [];
-            let seenKeys = {};
-            this.tableItems.forEach(element => {
-                for (const key in element) {
-                    if (seenKeys[key])
-                        continue;
-                    seenKeys[key] = true;
-                    headers.push({ value: key });
-                }
+            const headers = [];
+
+            // Store every unique header key
+            this.tableItems.forEach((item) => {
+                Object.keys(item).forEach((key) => {
+                    if (!headers.some((header) => header.value === key)) {
+                        headers.push({ value: key });
+                    }
+                });
             });
-            /* hook before final column setup */
-            this.headersComputation(headers, this.items);
-            /* setup columns and potentially discard hidden ones (splice), hence the reverse loop */
+
+            // Run custom processing of headers
+            this.customHeadersComputation(headers);
+
+            // Set up columns and potentially discard hidden ones (splice), hence the reverse loop
             let i = headers.length;
             while (i--) {
-                let h = headers[i];
-                let cdef = Object.assign({}, default_column_definition, this.$props.columnDefinition[h.value]);
-                if (cdef.hidden) {
+                const header = headers[i];
+                const columnDefinition = Object.assign(
+                    {},
+                    defaultColumnDefinition,
+                    this.$props.columnDefinition[header.value]
+                );
+
+                if (columnDefinition.hidden) {
                     headers.splice(i, 1);
                     continue;
                 }
-                if (h.text === undefined)
-                    h.text = h.value.charAt(0).toUpperCase() + h.value.slice(1);
-                h.divider = true;
-                h.cdef = cdef;
+
+                header.text =
+                    header.value === 'flag' ? '' : header.value.charAt(0).toUpperCase() + header.value.slice(1);
+                header.divider = true;
+                header.columnDefinition = columnDefinition;
             }
+
             return headers;
         },
-        tableData() {
+        formattedTableItems() {
             return this.tableItems.map((item, index) => ({
                 id: index,
-                ...item
+                ...item,
             }));
         },
     },
@@ -217,81 +234,97 @@ export default {
         return {
             tableItems: [],
             tableHeight: '',
-            loading: false,
-            tableFooterProps: {'items-per-page-options': [50, 100, 150, -1]},
-        }
+            isLoading: false,
+            tableFooterProps: { 'items-per-page-options': [50, 100, 150, -1] },
+        };
     },
     watch: {
-        api() { this.getTableItems(); }
+        api: {
+            immediate: true,
+            handler() {
+                this.fetchTableItems();
+            },
+        },
     },
     methods: {
-        getTableItems() {
-            console.log('autotable getItems apiUrl', this.$props.api)
-            this.loading = true;
-            axios({
-                method: 'get',
-                url: this.$props.api,
-            })
-                .then( (response) => {
-                    const path = this.$props.arrayData.split(".");
-                    let pointer = response.data;
+        /**
+         * Fetch the data to display in a table from the API
+         */
+        fetchTableItems() {
+            this.isLoading = true;
 
-                    if (path != "") {
+            this.$api
+                .get(this.$props.api)
+                .then((response) => {
+                    const path = this.$props.arrayData.split('.');
+                    let tableData = response.data;
+
+                    if (unArray(path) !== '') {
                         for (let i = 0; i < path.length; i++) {
-                            if (pointer[path[i]]) {
-                                pointer = pointer[path[i]];
+                            if (tableData[path[i]]) {
+                                tableData = tableData[path[i]];
                             } else {
-                                pointer = null;
+                                tableData = null;
                                 break;
                             }
                         }
-                        if (pointer==null) {
+                        if (tableData === null) {
                             this.$emit('error', 'Error occurs in data path.');
                         }
                     }
-                    this.tableItems = pointer;
-                    this.loading = false;
+                    this.tableItems = tableData;
+                    this.isLoading = false;
                     this.$emit('error', {});
-                }).catch( (error) => {
-                    this.tableItems = [];
-                    console.log('data from autoTable error', error);
-                    this.$emit('error', { type: 'error', content: 'Cannot load data, problem with the query.', error: error });
+                })
+                .catch((error) => {
+                    this.$emit('error', {
+                        type: 'error',
+                        content: 'Cannot load data, problem with the query.',
+                        error: error,
+                    });
                 });
         },
+        getRowClass(item) {
+            return typeof this.itemClass === 'function' ? this.itemClass(item) : this.itemClass;
+        },
+        getHeaderClassForItem(header, tableItem) {
+            return header.columnDefinition.getClass(tableItem) + ' v-data-table__divider col_' + header.value;
+        },
         setTableHeight() {
-            if (this.$props.height == 'auto')
-                this.tableHeight = this.computeAutoTableHeight();
-            else if (this.$props.height !== undefined)
-                this.tableHeight = this.$props.height;
+            if (this.$props.height === 'auto') this.tableHeight = this.computeAutoTableHeight();
+            else if (this.$props.height) this.tableHeight = this.$props.height;
         },
         computeAutoTableHeight() {
-            const table = document.getElementById(this.id);
             let tableHeight = 0;
+            const table = document.getElementById(this.id);
+
             if (table) {
                 const tableFooterElement = table.getElementsByClassName('v-data-footer');
                 const footerHeight = tableFooterElement.length ? tableFooterElement[0].clientHeight : 0;
-                if (table.parentElement && table.parentElement.style.height != '') {
+
+                if (table.parentElement && table.parentElement.style.height !== '') {
                     tableHeight += table.parentElement.style.height;
-                    console.log('mode parent', table.parentElement.style.height)
-                }
-                else {
+                } else {
                     const tableRect = table.getBoundingClientRect();
-                    tableHeight += (window.innerHeight - tableRect.top);
+                    tableHeight += window.innerHeight - tableRect.top;
                 }
+
                 tableHeight -= footerHeight;
-                if (this.autoTableHeightExtra) {
-                    for (let i of this.autoTableHeightExtra) {
-                        if (typeof i == 'number')
-                            tableHeight += i;
-                        else {
-                            i = document.getElementById(i);
-                            if (i)
-                                tableHeight -= i.clientHeight;
+
+                if (this.heightOffsets) {
+                    this.heightOffsets.forEach((offset) => {
+                        if (typeof offset === 'number') {
+                            tableHeight += offset;
+                        } else {
+                            const element = document.getElementById(offset);
+
+                            if (element) {
+                                tableHeight -= element.clientHeight;
+                            }
                         }
-                    }
+                    });
                 }
             }
-            console.log('auto-table computeAutoTableHeight()', tableHeight);
             return tableHeight;
         },
         /**
@@ -300,24 +333,24 @@ export default {
          * @param {number} index - item index
          */
         copyCellContent(value, index) {
-            let elCopy = document.getElementsByClassName(`${value}`)[index].innerText;
-            copyToClipboard(elCopy)
-                .then(() => {
-                    const tooltipElement = document.querySelector('.cp-span:hover .cell-copied-tooltip');
-                    if (tooltipElement) {
-                        tooltipElement.style = "visibility:visible;";
-                        setTimeout(() => { tooltipElement.style = ""; }, 400);
-                    }
-                });
-        }
+            const elCopy = document.getElementsByClassName(`${value}`)[index].innerText;
+            copyToClipboard(elCopy).then(() => {
+                const tooltipElement = document.querySelector('.cp-span:hover .cell-copied-tooltip');
+                if (tooltipElement) {
+                    tooltipElement.style = 'visibility:visible;';
+                    setTimeout(() => {
+                        tooltipElement.style = '';
+                    }, 400);
+                }
+            });
+        },
     },
     mounted() {
-        this.getTableItems();
         this.setTableHeight();
         window.addEventListener('resize', this.setTableHeight);
     },
     destroyed() {
         window.removeEventListener('resize', this.setTableHeight);
     },
-}
+};
 </script>
