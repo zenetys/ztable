08fef53f2d647bb93db3f14596fd557c58387d43
Pierre Delclaux <pimise@gmail.com>
2023-04-11 11:03:36 +0200

    MINOR: ConfigDialog: Improve visibility for smaller displays

--- src/components/ConfigDialog.vue
+++ src/components/ConfigDialog.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row justify="center">
-        <v-dialog v-model="$store.showConfigDialog" persistent max-width="600px">
+        <v-dialog v-model="$store.showConfigDialog" persistent scrollable max-width="600px">
             <v-card>
                 <v-card-title>
                     <span class="text-h5">Configuration</span>
